e774ee939895c912c55792ee0f3e5964cabb44be
giito <giito0101@gmail.com>
2019-10-08 00:27:06 +0900

    2-9実装途中

--- 2-9/2-9.js
+++ 2-9/2-9.js
@@ -24,9 +24,39 @@ var app2 = new Vue({
 });
 
 //一方通行のバインディング
-var app = new Vue({
+var app3 = new Vue({
 	el:'#app3',
 	data:{
 		message:'これは一方通行のバインドです。'
 	}
 })
+
+//チェックボックスに文字列をバインドする
+var app4 = new Vue({
+	el:'#app4',
+	data:{
+		answer:'はい'
+	}
+});
+
+//グループ化したチェックボックスにバインドする
+var app5 = new Vue({
+	el: '#app5',
+	data: {
+		answer: new Array()
+	},
+	computed: {
+		//チェック内容を連結した文字列を返す算出プロパティ
+		selection: function() {
+			return this.answer.join();
+		}
+	}
+});
+
+//ラジオボタンにバインドする
+var app = new Vue({
+	el: '#app',
+	data: {
+		answer: '選択してください'
+	}
+});
