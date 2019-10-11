ffcf8ad8598bb59b5a5c8f662d021060851fb49c
giito <giito0101@gmail.com>
2019-10-11 22:31:11 +0900

    2-10 transition実装

---
+++ 2-10/2-10.js
@@ -0,0 +1,23 @@
+//フェードイン・フェードアウト
+var app = new Vue({
+	el: '#app',
+	data: {
+		show: true
+	}
+});
+
+//name属性でエフェクトを分類
+var app2 = new Vue({
+	el: '#app2',
+	data: {
+		show:true
+	}
+});
+
+//カスタムトランジションクラスの使用例
+var app3 = new Vue({
+	el: '#app3',
+	data: {
+		show: true
+	}
+});
